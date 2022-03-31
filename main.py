c6a9a698e216fef04e90050cb0eebef2fec25a50
Thaina <thaina@uhedge.com.br>
2022-03-31 01:10:45 -0300

    verify vertex in same polygon

--- main.py
+++ main.py
@@ -33,72 +33,112 @@ def populate_data(file_lines):
     if break_lines and len(break_lines) == 3:
         vertices = read_vertices(break_lines[0])
         polygons = read_polygons(break_lines[1])
-        start = tuple(break_lines[2][0].split())
-        endpoint = tuple(break_lines[2][1].split())
+        vertices['start'] = tuple(map(int,break_lines[2][0].split()))
+        vertices['endpoint'] = tuple(map(int,break_lines[2][1].split()))
+        return (vertices, polygons)
 
-        return (vertices, polygons, start, endpoint)
-
-def verify_intersection(p1, p2, p3, p4):
-    max_x = max([p1[0], p2[0]])
-    min_x = min([p1[0], p2[0]])
-    if not (min_x <= p3[0] <= max_x or min_x <= p4[0] <= max_x):
+def does_intersect(a, b, c, d, p, q, r, s):
+    det = (c - a) * (s - q) - (r - p) * (d - b)
+    if (det == 0):
         return 0
-    
-    max_y = max([p1[1], p2[1]])
-    min_y = min([p1[1], p2[1]])
-    if not (min_y <= p3[1] <= max_y or min_y <= p4[1] <= max_y ):
-        return 0
-    
-    return 1
+    else:
+        lambda_ = ((s - q) * (r - a) + (p - r) * (s - b)) / det
+        gamma = ((b - d) * (r - a) + (c - a) * (s - b)) / det
+        return ((0 < lambda_ and lambda_ < 1) and (0 < gamma and gamma < 1)) * 1
 
-def line_equation(p1, p2):
-    try:
-        m = (p2[1] - p1[1]) / (p2[0] - p1[0])
-        n = p2[1] - m*p2[0]
-        y = lambda x: m*x + n
-    except ZeroDivisionError:
-        y = lambda x: p2[0]
-
-    return y
-
-def children_graph(vertices, polygons):
-    children_dict = {}
+def is_visible(vertices, polygons):
     vertices_list = vertices.keys()
-    edge_list = {}
-
-    for i, polygon in polygons.items():
+    visible = {}
+    edge_list = []
+    for polygon in polygons.values():
         for j in range(len(polygon) - 1):
-            func = line_equation(vertices[polygon[j]], vertices[polygon[j+1]])
-            edge_list[polygon[j]+polygon[j+1]] = func
+            edge_list.append(polygon[j]+polygon[j+1])
             try:
-                children_dict[polygon[j]].append(polygon[j+1])
+                visible[polygon[j]].append(polygon[j+1])
             except:
-                children_dict[polygon[j]] = [polygon[j+1]]
+                visible[polygon[j]] = [polygon[j+1]]
                 pass
             try:
-                children_dict[polygon[j+1]].append(polygon[j])
+                visible[polygon[j+1]].append(polygon[j])
             except:
-                children_dict[polygon[j+1]] = [polygon[j]]
-                pass
-        
-    for vertice, position in vertices.items():
+                visible[polygon[j+1]] = [polygon[j]]
+    
+    polygon_dict = {}
+
+    xB = -10
+    yB = 1000
+    for i, polygon in polygons.items():
+        for vertex in polygon:
+            polygon_dict[vertex] = i
+            xAaux = vertices[vertex][0]
+            yAaux = vertices[vertex][1]
+            for vertex_2 in polygon:
+                if vertex == vertex_2: continue
+                if vertex_2 in visible[vertex]: continue
+                counter = 0
+                xBaux = vertices[vertex_2][0]
+                yBaux = vertices[vertex_2][1]
+                xA = (xAaux + xBaux) / 2
+                yA = (yAaux + yBaux) / 2
+                for edge in edge_list:
+                    xC = vertices[edge[0]][0]
+                    yC = vertices[edge[0]][1]
+                    xD = vertices[edge[1]][0]
+                    yD = vertices[edge[1]][1]
+                    is_inter = does_intersect(xA, yA, xB, yB, xC, yC, xD, yD)
+                    is_inter = does_intersect(xA, yA, xB, yB, xC, yC, xD, yD)
+                    if is_inter:
+                        counter += 1
+                if counter%2 == 0:
+                    try:
+                        visible[vertex].append(vertex_2)
+                    except:
+                        visible[vertex] = [vertex_2]
+                    try:
+                        visible[vertex_2].append(vertex)
+                    except:
+                        visible[vertex_2] = [vertex]
+
+    polygon_dict['start'] = -1
+    polygon_dict['endpoint'] = -2
+
+    for vertice in vertices_list:
+        xA = vertices[vertice][0]
+        yA = vertices[vertice][1]
         for vert in vertices_list:
             if vert == vertice: continue
-            if vert in children_dict[vertice]: continue
-            for edge in edge_list.keys():
-                print(edge)
-                print(vertice, vert)
-                is_inter = verify_intersection(vertices[edge[0]], vertices[edge[1]], vertices[vertice], vertices[vert])
-
-                print(is_inter)
-    print(children_dict)
+            if vertice in visible.keys() and vert in visible[vertice]: continue
+            if polygon_dict[vert] == polygon_dict[vertice]: continue
+            xB = vertices[vert][0]
+            yB = vertices[vert][1]
+            intersect = []
+            for edge in edge_list:
+                xC = vertices[edge[0]][0]
+                yC = vertices[edge[0]][1]
+                xD = vertices[edge[1]][0]
+                yD = vertices[edge[1]][1]
+                is_inter = does_intersect(xA, yA, xB, yB, xC, yC, xD, yD)
+                intersect.append(is_inter)
+                if is_inter: 
+                    break
+            if 1 not in intersect:
+                try:
+                    visible[vertice].append(vert)
+                except:
+                    visible[vertice] = [vert]
+                try:
+                    visible[vert].append(vertice)
+                except:
+                    visible[vert] = [vertice]
+    return visible
 
 file_lines = read_file()
-vertices, polygons, start, endpoint = populate_data(file_lines)
-children_graph(vertices, polygons)
+
+vertices, polygons = populate_data(file_lines)
+
+visible_graph = is_visible(vertices, polygons)
 print(vertices)
 
 print(polygons)
 
-print('Start point: ', start)
-print('End point: ', endpoint)
+print(visible_graph)
